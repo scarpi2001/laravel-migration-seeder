597330c8f2cd9e65f5a525103688a827fa49bad8
Marco <posta.email3001@gmail.com>
2023-02-01 17:24:34 +0100

    creata migration e generata table trains

--- database/migrations/2023_02_01_155733_create_trains_table.php
+++ database/migrations/2023_02_01_155733_create_trains_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,11 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string("code", 10)->unique();
+            $table->string("company_name", 64);
+            $table->smallInteger('wagons')->nullable(true)->default(2)->unsigned();
+            $table->string("type", 32);
+            $table->year("production_date")->nullable(true);
             $table->timestamps();
         });
     }
