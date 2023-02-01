d1ac7922e7ab910e8c640aa3f183a86ee2fa9c8d
Marco <posta.email3001@gmail.com>
2023-02-01 17:39:16 +0100

    generata table travel

--- database/migrations/2023_02_01_155827_create_travel_table.php
+++ database/migrations/2023_02_01_155827_create_travel_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('travel', function (Blueprint $table) {
             $table->id();
+            $table->string("code", 10)->unique();
+            $table->string("destination", 64);
+            $table->string("departure_station", 128);
+            $table->string("arrival_station", 128);
+            $table->smallInteger("travel_duration");
+            $table->smallInteger("price");
             $table->timestamps();
         });
     }
